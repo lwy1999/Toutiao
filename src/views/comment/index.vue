9cc465d9cea0ba401a4ea6f46c9a3f372e5f9f5e
lwy1999 <15035496608@163.com>
2019-11-18 21:20:56 +0800

    修改评论状态

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
     <el-card class="box-card">
         <div slot="header" class="clearfix">
             <span>评论管理</span>
@@ -28,7 +29,8 @@
                     <el-switch
                         v-model="scope.row.comment_status"
                         active-color="#13ce66"
-                        inactive-color="#ff4949">
+                        inactive-color="#ff4949"
+                        @change="onStatusChange(scope.row)">
                     </el-switch>
                 </template>
             </el-table-column>
@@ -42,6 +44,7 @@
             </el-table-column>
         </el-table>
     </el-card>
+</div>
 </template>
 
 <script>
@@ -68,6 +71,26 @@ export default {
       }).then(res => {
         // console.log(res)
         this.articles = res.data.data.results
+      })
+    },
+    onStatusChange (article) {
+      this.$axios({
+        method: 'PUT',
+        url: '/comments/status',
+        params: {
+          article_id: article.id.toString()
+        },
+        data: {
+          allow_comment: article.comment_status
+        }
+      }).then(res => {
+        // console.log(res)
+        this.$message({
+          type: 'success',
+          message: `${article.comment_status ? '启用' : '关闭'}成功`
+        })
+      }).catch(() => {
+        this.$message.erroe('操作失败')
       })
     }
   }
